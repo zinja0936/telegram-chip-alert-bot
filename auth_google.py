0bbccb1ff65c9cff23812d451af80af7d1c405c9
zinja0936 <37123317+zinja0936@users.noreply.github.com>
2025-06-17 10:12:17 +0800

    Update auth_google.py

--- auth_google.py
+++ auth_google.py
@@ -1,17 +1,24 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+
 def auth_google():
-    scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive"]
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive"
+    ]
     creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
     return gspread.authorize(creds)
 
-def get_sheet_data(sheet_url):
+
+def get_sheet_data(sheet_id):
     gc = auth_google()
-    sheet = gc.open_by_key(sheet_url).sheet1
+    sheet = gc.open_by_key(sheet_id).sheet1
     return sheet.get_all_values()
 
-def update_sheet_note(sheet_url, row, col, note):
+
+def update_sheet_note(sheet_id, row, col, note):
     gc = auth_google()
-    sheet = gc.open_by_key(sheet_url).sheet1
+    sheet = gc.open_by_key(sheet_id).sheet1
     sheet.update_cell(row, col, note)

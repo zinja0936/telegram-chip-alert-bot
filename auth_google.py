09c43a9808c3ae41b2710056235b9d26d887e50f
zinja0936 <37123317+zinja0936@users.noreply.github.com>
2025-06-25 09:53:15 +0800

    Update auth_google.py

--- auth_google.py
+++ auth_google.py
@@ -1,6 +1,7 @@
+import os
+import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-
 
 def auth_google():
     scope = [
@@ -8,15 +9,18 @@ def auth_google():
         "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive"
     ]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
-    return gspread.authorize(creds)
 
+    # 從環境變數中讀取 JSON 金鑰字串，轉為 dict
+    google_json_str = os.environ.get("GOOGLE_JSON")
+    creds_dict = json.loads(google_json_str)
+
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+    return gspread.authorize(creds)
 
 def get_sheet_data(sheet_id):
     gc = auth_google()
     sheet = gc.open_by_key(sheet_id).sheet1
     return sheet.get_all_values()
-
 
 def update_sheet_note(sheet_id, row, col, note):
     gc = auth_google()
